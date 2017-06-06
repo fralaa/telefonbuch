b69d3b48032646f263c3eeea96de91c5573a8034
fralaa <franziska.laar@student.fhws.de>
2017-06-06 14:58:25 +0200

    adds json syntax

--- telefonbuch/build/source/telefonbuch.java
+++ telefonbuch/build/source/telefonbuch.java
@@ -14,17 +14,13 @@ import java.io.IOException;
 
 public class telefonbuch extends PApplet {
 
-String name01 = "Adam Apfel";
-String name02 = "Bernd Birne";
-String name03 = "Christine Carl";
-
-String number01 = "0123 456789";
-String number02 = "0789 123456";
-String number03 = "0456 789123";
+JSONArray list;
 
 public void setup(){
 
-background(0);
+
+
+list = loadJSONArray("json/list.json");
 }
 
 public void draw(){
@@ -35,9 +31,15 @@ background(255);
 text("Telefonbuch",20, 40);
 
 textSize(12);
-text(name01 + ":  " + number01 ,20 ,80);
-text(name02 + ":  " + number02 ,20 ,110);
-text(name03 + ":  " + number03 ,20 ,140);
+for(int i = 0; i < list.size(); i++){
+  JSONObject entry = list.getJSONObject(i);
+  text(entry.getString("name") + ":  " + entry.getString("number"), 20, 80 + i *30);
+}
+
+}
+
+public void mousePressed(){
+  list = loadJSONArray("json/list.json");
 
 }
   public void settings() { 
--- telefonbuch/telefonbuch.pde
+++ telefonbuch/telefonbuch.pde
@@ -1,26 +1,29 @@
-String name01 = "Adam Apfel";
-String name02 = "Bernd Birne";
-String name03 = "Christine Carl";
-
-String number01 = "0123 456789";
-String number02 = "0789 123456";
-String number03 = "0456 789123";
+JSONArray list;
 
 void setup(){
 size(400, 800);
-background(0);
+
+
+list = loadJSONArray("json/list.json");
 }
+
 
 void draw(){
 background(255);
 
   fill(0);
   textSize(24);
-text("Telefonbuch",20, 40);
+  text("Telefonbuch",20, 40);
 
 textSize(12);
-text(name01 + ":  " + number01 ,20 ,80);
-text(name02 + ":  " + number02 ,20 ,110);
-text(name03 + ":  " + number03 ,20 ,140);
+for(int i = 0; i < list.size(); i++){
+  JSONObject entry = list.getJSONObject(i);
+  text(entry.getString("name") + ":  " + entry.getString("number"), 20, 80 + i *30);
+}
+}
+
+
+void mousePressed(){
+  list = loadJSONArray("json/list.json");
 
 }
